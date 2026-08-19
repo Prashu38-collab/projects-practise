fc3ae45acbe4993c0065a56e9b3a8565c7d10ff2
Prashamsa Ghimire <ghimireprashamsa26@gmail.com>
2026-08-20 00:13:34 +0545

    Fixed voice issue

--- Text_Speech/audio_pdf.py
+++ Text_Speech/audio_pdf.py
@@ -1,19 +1,22 @@
 import pyttsx3
 import PyPDF2
 
-# read book in binary format
-with open('/Users/prashamsaghimire/Desktop/Projects/Text_Speech/aboutMe.pdf','rb') as file:
-    reading_pdf=PyPDF2.PdfReader(file)
+with open('/Users/prashamsaghimire/Desktop/Projects/Text_Speech/aboutMe.pdf', 'rb') as file:
 
-# number of pages
+    reading_pdf = PyPDF2.PdfReader(file)
+
     pdf_pages = len(reading_pdf.pages)
 
-# Convert it to speech 
+    choose_page = reading_pdf.pages[0]
 
-pdf_speaker=pyttsx3.init() #initialize library
-#  IT WILL READ INDEX WISE SO FOR READING 4 PAGE- PROVIDE 3
-choose_page=reading_pdf.pages[0]
-pdf_text=choose_page.extract_text()
-print(pdf_text)
+    pdf_text = choose_page.extract_text()
+
+pdf_speaker = pyttsx3.init()
+
 pdf_speaker.say(pdf_text)
+pdf_speaker.runAndWait()
+
+# save to aiff- mac supports this
+
+pdf_speaker.save_to_file(pdf_text,'/Users/prashamsaghimire/Desktop/Projects/Text_Speech/aboutMe.aiff')
 pdf_speaker.runAndWait()
