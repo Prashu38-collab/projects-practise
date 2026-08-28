28b775e3be29731c73ada5a7050b93a1b673e2dc
Prashamsa Ghimire <ghimireprashamsa26@gmail.com>
2026-08-28 22:20:17 +0545

    Added activity records that browser activity into structured records.

--- Study_Activity_Analyser/extension/background.js
+++ Study_Activity_Analyser/extension/background.js
@@ -1,4 +1,5 @@
 let startTime = null;
+let currentTab=null;
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
 
@@ -9,6 +10,15 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
         let duration = endTime - startTime;
         let durationSeconds = duration / 1000;
 
+        // activity records
+        let activity={
+            title:currentTab.title,
+            url:currentTab.url,
+            duration:duration
+        };
+        console.log("Activity: ",activity)
+
+
         console.log("Time spent:", durationSeconds, "seconds");
     }
 
@@ -17,10 +27,10 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
     chrome.tabs.query(
         { active: true, currentWindow: true },
         function(tabs) {
+            currentTab = tabs[0];
+            console.log("Current tab:", currentTab.title);
 
-            console.log("Title:", tabs[0].title);
-            console.log("URL:", tabs[0].url);
-            console.log("Start time:", startTime);
+            
         }
     );
 
