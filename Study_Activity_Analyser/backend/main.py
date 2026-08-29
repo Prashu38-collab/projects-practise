e1fd053099cbf72cc300f5b4a058ab5058c25d55
Prashamsa Ghimire <ghimireprashamsa26@gmail.com>
2026-08-29 15:15:03 +0545

    Replaced list with dataabse with orm model implementation

--- Study_Activity_Analyser/backend/main.py
+++ Study_Activity_Analyser/backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 app=FastAPI()
 # Allow requests from the Chrome extension
@@ -10,12 +12,36 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-activities = []
+
+# Database connectiion and setup
+DATABASE_URL = "sqlite:///study_activity.db"
+engine= create_engine(DATABASE_URL, echo=True)
+
+# creating session - it is used to interact with the database
+Session=sessionmaker(bind=engine)
+Base=declarative_base()
+#  stdy Activity table model usinfg orm -
+class StudyActivity(Base):
+    __tablename__ = "study_activities"
+    id=Column(Integer,primary_key=True)
+    title=Column(String)
+    url=Column(String)
+    duration=Column(Float)
+# Create the table in the database
+Base.metadata.create_all(engine)
 
 # post function - that recives the json file from chrome and add to activity which is a dict
 @app.post("/activity")
 def receive_activity(activity: dict): #this is a typehint
-    activities.append(activity)
+    db_session = Session()
+    study_activity = StudyActivity(
+        title=activity.get("title"),
+        url=activity.get("url"),
+        duration=activity.get("duration")
+    )
+    db_session.add(study_activity)
+    db_session.commit()
+    db_session.close()
     print("Received activity:")
     print(activity)
 
@@ -23,4 +49,7 @@ def receive_activity(activity: dict): #this is a typehint
 # get activities from what is stored in list activities to verify what is present.
 @app.get("/activities")
 def get_activities():
+    db_session = Session()
+    activities = db_session.query(StudyActivity).all()
+    db_session.close()
     return activities
