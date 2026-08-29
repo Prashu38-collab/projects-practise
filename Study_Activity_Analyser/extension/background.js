c7f4da16e27ca03a5cda9fc1a6854cf83383a6fe
Prashamsa Ghimire <ghimireprashamsa26@gmail.com>
2026-08-29 14:34:30 +0545

    added temporary list to store activities

--- Study_Activity_Analyser/extension/background.js
+++ Study_Activity_Analyser/extension/background.js
@@ -14,23 +14,32 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
         let activity={
             title:currentTab.title,
             url:currentTab.url,
-            duration:duration
+            duration:durationSeconds
         };
-        console.log("Activity: ",activity)
+        console.log("Activity: ",activity);
 
 
         console.log("Time spent:", durationSeconds, "seconds");
+        // Send activity to Python
+        fetch("http://127.0.0.1:8000/activity", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(activity)
+        });
     }
 
+    // Start timer for the new tab
     startTime = Date.now();
 
     chrome.tabs.query(
         { active: true, currentWindow: true },
         function(tabs) {
-            currentTab = tabs[0];
-            console.log("Current tab:", currentTab.title);
 
-            
+            currentTab = tabs[0];
+
+            console.log("Current tab:", currentTab.title);
         }
     );
 
