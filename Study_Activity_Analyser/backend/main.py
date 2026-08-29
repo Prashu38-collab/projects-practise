0d35693ca23a55a47dd09f51367313b283b047a1
Prashamsa Ghimire <ghimireprashamsa26@gmail.com>
2026-08-29 14:33:16 +0545

    fixed cors error

--- Study_Activity_Analyser/backend/main.py
+++ Study_Activity_Analyser/backend/main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 app=FastAPI()
+# Allow requests from the Chrome extension
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+activities = []
 
 # post function - that recives the json file from chrome and add to activity which is a dict
 @app.post("/activity")
 def receive_activity(activity: dict): #this is a typehint
+    activities.append(activity)
     print("Received activity:")
     print(activity)
 
     return {"status": "received"}
+# get activities from what is stored in list activities to verify what is present.
+@app.get("/activities")
+def get_activities():
+    return activities
